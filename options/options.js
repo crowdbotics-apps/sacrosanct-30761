5d2b5ac0cc3d162476154885902036dae2d7128e
Crowdbotics <team@crowdbotics.com>
2021-09-23 00:01:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sacrosanct_30761Identifier",
+  url: "https://sacrosanct_30761Identifier.botics.co",
+  api: "https://sacrosanct_30761Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
